8df67c1f84d0ed723435d04348d296c4e5384950
Nicholas Prodehl <nicholasprodehl@gmail.com>
2021-10-26 15:02:30 -0500

    Listen for the submit event on the entry form. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 /* global data */
 /* exported data */
+var $photoURL = document.querySelector('#photoURL');
+var $img = document.querySelector('img');
+function handlePhoto(event) {
+  $img.setAttribute('src', event.target.value);
+}
+$photoURL.addEventListener('input', handlePhoto);
+
+// var dataObj = {};
+
+// function handleSubmit(event){
+
+// }
+// document.addEventListener('submit', handleSubmit);
